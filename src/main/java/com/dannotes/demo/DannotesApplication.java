40663a87b3b764b38814aec5396253d1cae14e74
Danilozxz <progdanilo@gmail.com>
2024-10-12 20:30:41 -0300

    chore: mudança em nome da pasta

--- src/main/java/com/dannotes/demo/DannotesApplication.java
+++ src/main/java/com/dannotes/demo/DannotesApplication.java
@@ -1,4 +1,4 @@
-package com.dannotes.dannotes;
+package com.dannotes.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
