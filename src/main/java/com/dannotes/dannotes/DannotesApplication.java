81f475105fe1afcf8d70f5fc439c07a10dbd5d3b
Danilozxz <progdanilo@gmail.com>
2024-10-12 20:18:58 -0300

    chore: estrutura spring boot

---
+++ src/main/java/com/dannotes/dannotes/DannotesApplication.java
@@ -0,0 +1,13 @@
+package com.dannotes.dannotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DannotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dannotes/dannotes/DannotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannotes.dannotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DannotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
